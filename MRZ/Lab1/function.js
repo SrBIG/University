951ca75c3e56b6f7161f04b7bfecf8ba79ce020f
SrBIG <igb9925@gmail.com>
2018-05-06 20:40:40 +0300

    add MRZ

---
+++ MRZ/Lab1/function.js
@@ -0,0 +1,165 @@
+function binaryToDecimal(n) {
+    n = parseInt(n, 2);
+    return n;
+}
+
+function decimalToBinary(n) {
+    n = Number(n);
+    n = (n >>> 0).toString(2);
+    return n;
+}
+
+function printBinary(n) {
+    var str = String(n);
+    if (str.length % 4) {
+        if (str.length % 4 === 1) {
+            str = "000" + str;
+        }
+        if (str.length % 4 === 2) {
+            str = "00" + str;
+        }
+        if (str.length % 4 === 3) {
+            str = "0" + str;
+        }
+
+    }
+    str = str.replace(/(\d)(?=(\d\d\d\d)+([^\d]|$))/g, '$1 ');
+
+    if (( /(?=(0000\s)+([\d]|$))/).test(str)){
+        str = str.substring(5);
+    }
+    return str;
+}
+
+function printBinaryRes(str){
+    if ((str.length % 4)){
+        if (str.length % 4 === 1) {
+        return("000" + str.substring(0, 1) + " " + str.substring(1));
+    }
+        if (str.length % 4 === 2) {
+            return("00" + str.substring(0, 2) + " " + str.substring(2));
+        }
+        if (str.length % 4 === 3) {
+            return("0" + str.substring(0, 3) + " " + str.substring(3));
+        }
+    }
+    return ("0000 "+str);
+}
+
+function inputIsArrayOfNumbers(str){
+    return (( /(^[\d\s]+$)/.test(str)));
+}
+
+function inputIsNumber(str){
+    return (( /(^[\d]+$)/.test(str)));
+}
+
+function getBoolArray(str){
+    var arr = String(str);
+    arr = str.split("");
+    for (var i = 0; i < arr.length; i++){
+        arr[i] = Number(arr[i]); // т.к. строка всегда true
+        arr[i] = Boolean(arr[i]);
+    }
+    return arr;
+}
+
+function multiply1Bin(a, b){
+    var c = [];
+    for (var i = 0; i < a.length; i++){
+        if (b == 1){
+            if (a[i] == 0){
+                c[i] = 0;
+            } else {
+                c[i] = 1;
+            }
+        }
+        if (b == 0){
+            c[i] = 0;
+        }
+    }
+    return c;
+}
+
+function shiftLeft(c){
+    c.push(0);
+    return c;
+}
+
+function binarySum(a, b){
+    if (a.length > b.length){
+        var dif = a.length - b.length;
+        for (var i = 0; i < dif; i++){
+            b.unshift(false);
+        }
+    }
+    if (b.length > a.length){
+        var dif = b.length - a.length;
+        for (var i = 0; i < dif; i++){
+            a.unshift(false);
+        }
+    }
+
+    var c = [];
+    var d = 0;
+
+    for (var i = a.length - 1; i >= 0; i--) {
+        if (a[i] == 0 && b[i] == 0) {
+            if (d == 1){
+                c[i] = 1;
+                d = 0;
+            } else {
+                c[i] = 0;
+                d = 0;
+            }
+        } else {
+            if ((a[i] == 1 && b[i] == 0) || (a[i] == 0 && b[i] == 1)) {
+                if (d == 1) {
+                    c[i] = 0;
+                    d = 1;
+                } else {
+                    c[i] = 1;
+                    d = 0;
+                }
+            } else {
+                if (a[i] == 1 && b[i] == 1) {
+                    if (d == 1) {
+                        c[i] = 1;
+                        d = 1;
+                    } else {
+                        c[i] = 0;
+                        d = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    if (d == 1){
+        c.unshift(1);
+    }
+
+    return c;
+}
+
+function resToString(c){
+    for (var i = 0; i < c.length; i++){
+        c[i] = Number(c[i]);
+        c[i] = String(c[i]);
+    }
+    c = c.join("");
+    //alert("c: "+c);
+    return c;
+}
+
+function binaryMultiply(a,b){
+    var partSum = [];
+    var res = [];
+    for (var i = 0; i < b.length; i++){
+        res = shiftLeft(res);
+        partSum = multiply1Bin(a, b[i]);
+        res = binarySum(res, partSum);
+    }
+    return res;
+}
+
---
+++ MRZ/Lab1/handleButtonClick.js
@@ -0,0 +1,182 @@
+function handleButtonClick() {
+    var time = 0;
+    if (!document.getElementById("inputT").value ||
+        !document.getElementById("inputA").value ||
+        !document.getElementById("inputB").value) {
+        alert("Please fill in all the text fields!");
+        return 0;
+    }
+    var arrA = document.getElementById("inputA").value;
+    var arrB = document.getElementById("inputB").value;
+
+    var t = document.getElementById("inputT").value;
+
+    if (!inputIsNumber(t)){
+        alert("Please input numbers in the fields!");
+        return 0;
+    } else {
+        t = Number(t);
+    }
+
+    if (!inputIsArrayOfNumbers(arrA) || !inputIsArrayOfNumbers(arrB)){
+        alert("Please input numbers in the fields!");
+        return 0;
+    }
+
+    arrA = arrA.split(" ");
+    arrB = arrB.split(" ");
+    if (arrA.length != arrB.length) {
+        alert("Please input equal number of elements");
+        return 0;
+    }
+    var m = arrA.length;
+
+    var table = document.getElementById("table");
+    var rowCount = table.getElementsByTagName("tr").length;
+    if (rowCount > 1) {
+        for (var rowIndex = rowCount - 1; rowIndex > 0; rowIndex--) {
+            table.deleteRow(rowIndex);
+        }
+    }
+    
+    for (var i = 0; i < arrA.length; i++) {
+        if (!inputIsArrayOfNumbers(arrA[i])){
+            alert("Please input numbers in the fields!");
+            return 0;
+        }
+        arrA[i] = Number(arrA[i]);
+        if (arrA[i] > 15 || arrA[i] < 0) {
+            alert("Please input number in the [0; 15] interval!");
+            return 0;
+        }
+        arrA[i] = printBinary(decimalToBinary(arrA[i]));
+    }
+    for (var i = 0; i < arrB.length; i++) {
+        if (!inputIsArrayOfNumbers(arrB[i])){
+            alert("Please input numbers in the fields!");
+            return 0;
+        }
+        arrB[i] = Number(arrB[i]);
+        if (arrB[i] > 15 || arrB[i] < 0) {
+            alert("Please input number in the [0; 15] interval!");
+            return 0;
+        }
+        arrB[i] = printBinary(decimalToBinary(arrB[i]));
+    }
+
+    for (var rowIndex = 1; rowIndex <= 6+m; rowIndex++) {
+        i = rowIndex - 1;
+        var row = table.insertRow();
+
+        for (var cellIndex = 0; cellIndex < 9; cellIndex++) {
+            row.insertCell(cellIndex);
+        }
+    }
+
+    var boolArrA = [];
+    var boolArrB = [];
+    var arrC = [];
+
+    for (var index = 0; index < m; index++) {
+        boolArrA[index] = getBoolArray(arrA[index]);
+        boolArrB[index] = getBoolArray(arrB[index]);
+    }
+
+    var res = [];
+    var previousRes = [];
+    var cellIndex = 0;
+    var rowIndex = 0;
+    var step = [];
+    var time = 0;
+    for (var i = 0; i < m; i++){
+        step[i] = 0;
+        res[i] = [];
+        previousRes[i] = [];
+    }
+
+    for (var elemIndex = 0; elemIndex < m; elemIndex++){
+
+
+        time+=t;
+
+        if (elemIndex < m-1 && step[elemIndex] == 0){
+            rowIndex = elemIndex+1;
+            var resCell = table.rows[rowIndex].cells[0];
+            resCell.innerHTML = "A["+elemIndex+"] = "+ binaryToDecimal(resToString(boolArrA[elemIndex]))+"<br>"+
+                "B["+elemIndex+"] = "+ binaryToDecimal(resToString(boolArrB[elemIndex]))+"<br>"+"<b>Queue:</b><br>";
+            for (var k = elemIndex+1; k < m; k++){
+                resCell.innerHTML+="A["+k+"] = "+ binaryToDecimal(resToString(boolArrA[k]))+", "+
+                    "B["+k+"] = "+ binaryToDecimal(resToString(boolArrB[k]))+"<br>";
+            }
+        }
+
+        for (var j = elemIndex; j >= 0; j--){
+            if (step[j] < 8){
+                cellIndex = step[j]+1;
+                previousRes[j] = printBinaryRes(resToString(res[j]));
+                res[j] = pipeline(res[j], boolArrA[j], boolArrB[j], step[j]);
+                if (step[j] == 0){
+                    previousRes[j] = "0000 "+previousRes[j];
+                }
+                rowIndex = j + step[j] + 1;
+
+                if (step[j] == 7){
+                    var resCell = table.rows[rowIndex-1].cells[cellIndex];
+                    resCell.innerHTML = "<b>A:</b> "+binaryToDecimal(resToString(boolArrA[j]))+
+                        "<br>"+"<b>B:</b> "+binaryToDecimal(resToString(boolArrB[j]))+
+                        "<br>"+"<b>Result: </b>"+binaryToDecimal(resToString(res[j]));
+                } else {
+                    var resCell = table.rows[rowIndex].cells[cellIndex];
+                    resCell.innerHTML = "<b>A:</b> "+printBinary(resToString(boolArrA[j]))+
+                        "<br>"+"<b>B:</b> "+printBinary(resToString(boolArrB[j]))+
+                        "<br>"+
+                            "<b>Previous result: </b>"+previousRes[j]+"<br>"+
+                        "<b>Result: </b>"+printBinaryRes(resToString(res[j]));
+                }
+                step[j]++;
+            }
+        }
+    }
+
+    while (step[m-1] < 8){
+        time+=t;
+
+        for (var j = m-1; j >= 0; j--){
+            if (step[j] < 8){
+                cellIndex = step[j]+1;
+                previousRes[j] = printBinaryRes(resToString(res[j]));
+                res[j] = pipeline(res[j], boolArrA[j], boolArrB[j], step[j]);
+                rowIndex = j + step[j] + 1;
+
+                if (step[j] == 7){
+                    rowIndex--;
+                    var resCell = table.rows[rowIndex].cells[cellIndex];
+                    resCell.innerHTML = "<b>A:</b> "+binaryToDecimal(resToString(boolArrA[j]))+
+                        "<br>"+"<b>B:</b> "+binaryToDecimal(resToString(boolArrB[j]))+
+                        "<br>"+"<b>Result: </b>"+binaryToDecimal(resToString(res[j]));
+                } else {
+                    var resCell = table.rows[rowIndex].cells[cellIndex];
+                    resCell.innerHTML = "<b>A:</b> "+printBinary(resToString(boolArrA[j]))+
+                        "<br>"+"<b>B:</b> "+printBinary(resToString(boolArrB[j]))+
+                        "<br>"+
+                        "<b>Previous result: </b>"+previousRes[j]+"<br>"+
+                        "<b>Result: </b>"+printBinaryRes(resToString(res[j]));
+                }
+                step[j]++;
+            }
+        }
+    }
+    var timeOutput = 0;
+    for (var rowIndex = 1; rowIndex <= 6+m; rowIndex++) {
+        var resCell = table.rows[rowIndex].cells[0];
+        timeOutput +=t;
+        resCell.innerHTML += "<br><b>Time:</b> "+timeOutput;
+
+    }
+
+    for (var i = 0; i < res.length; i++){
+        arrC[i] = binaryToDecimal(resToString(res[i]));
+    }
+
+    document.getElementById("result").value = arrC;
+}
